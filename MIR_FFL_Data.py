12d90b18e6bfbf3801df4ea1df5e67a2952440b5
rclouser <30866149+rclouser@users.noreply.github.com>
2017-08-10 12:57:53 -0400

    Initial version
    Outputted source to a txt file. There are 2 tables in the page but currently can only get data from one.

---
+++ MIR_FFL_Data.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import time
+
+
+errorFile = open('C:\Python27\Projects\MIR_FFL\output\ERROR-MIRData.txt','w')
+outputFile = open('C:\Python27\Projects\MIR_FFL\output\MIRData.txt','w')
+print('start')
+
+def strip_html(colNum):
+	if colNum == 1 :
+		return col[colNum].a.string.strip()
+	else:
+		return col[colNum].string.strip()
+
+print('1')
+
+try:
+##	soup = BeautifulSoup('C:\Python27\Projects\MIR_FFL\output\htmlSourceFile.txt','html.parser')
+	soup = BeautifulSoup(open("C:\Python27\Projects\MIR_FFL\output\htmlSourceFile.txt"),"html.parser")
+
+	print('2')
+	
+	tableStats = soup.find("table", {"class" : "playerTableTable tableBody"})
+	for row in tableStats.findAll('tr') [3:-1]: ##first 3 rows are headers and don't need the last total row
+		col = row.findAll('td')
+		print('3-for')
+		try:
+			##name = col[0].a.string.strip()
+			name = strip_html(0) ##couldn't get encode to work here. so cleanup and output are in 2 steps
+			##outputFile.write(name.encode('utf-8')+'\n')
+			outputFile.write(name.encode('utf-8'))
+			opp = strip_html(1)
+			outputFile.write(',' + opp.encode('utf-8'))
+			pts = strip_html(3)
+			outputFile.write(',' + pts.encode('utf-8') + '\n')
+			
+		except Exception as e:
+			errorFile.write (str(e) + '***' + str(col) + '\n')
+			print(e)
+			pass
+	print('4')
+except Exception as ex:
+	errorFile.write("Exception: " + str(ex))
+	print(ex)
+	outputFile.close
+	errorFile.close
+
+
+print('5')	
+outputFile.close
+errorFile.close
+
